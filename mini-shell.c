5faa6f69c14ab4dbf8071327eb65c442a9752ac1
art@-v1r0s <arthurd2000@gmail.com>
2021-01-07 15:30:22 +0100

    add fork and wait for the father

--- mini-shell.c
+++ mini-shell.c
@@ -5,12 +5,18 @@
 #include <stdlib.h> // EXIT_SUCCESS
 #include <stdio.h>  // printf
 #include <string.h>
+
+#include <sys/wait.h> // wait
 #include <sys/types.h> // pid_t
 #include <unistd.h>    // fork
 
-int main(int argc, char *argv[])
+
+int main(int argc, char *argv[],char *env[])
 {
+    pid_t   pid;
+    int status;
     int i;
+    char shell[25]="/bin/";
     char command[25]={};
     for (i=1; i < argc; i++)
     {
@@ -18,8 +24,35 @@ int main(int argc, char *argv[])
         strcat(command,argv[i]);
         strcat(command, " ");
     }
+    printf("My pid : %d\n",getpid()); 
     printf("%s",command);
-    system(command);
+
+    //si il y a un argument
+    if (argc > 1 )
+    {
+
+        if ((pid = fork())== -1)
+        {
+            perror("fork");
+            return EXIT_FAILURE; 
+
+        }
+        //si pid = 0 alors on est dans le process fils
+        else if (pid == 0)
+        {
+
+        strcat(shell,argv[1]);
+        if (execve(shell, argv + 1,env )== -1)
+            perror("execve");
+
+        return EXIT_FAILURE; //on termine le fils meme si execve fail 
+        }
+        //dans le pere 
+        else
+            wait(&status);        
+    }
+
+    //system(command);
     getchar();
     return EXIT_SUCCESS;
 }
