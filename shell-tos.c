334d36c8f42c3e8217d9fb5365a3f4bbc17e360c
art@-v1r0s <arthurd2000@gmail.com>
2021-02-07 19:54:52 +0100

    finally v1

--- shell-tos.c
+++ shell-tos.c
@@ -1,21 +1,21 @@
 // base pour faire SON shell ;p
-// gcc -g -Wall -Wextra -pedantic shell-tos.c -o shell-tos
+// gcc -g -Wall -Wextra -fanalyzer -pedantic shell-tos.c -o shell-tos
 #include <stdlib.h> // EXIT_SUCCESS
 #include <stdio.h>  // printf
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    char command[40]="./mini-shell ";
+    char command[40]="./shellexecvp ";
     char argument[25]={};
     char  *result;
     do
     {
-        printf("Donnez votre commande\n");
+        printf("Shell-TOS=>\n");
         strcpy(argument,"");
-        strcpy(command,"./mini-shell ");
+        strcpy(command,"./shellexecvp ");
         fgets(argument,25,stdin);
         strcat(command,argument);
-        result = strstr(command, "./mini-shell exit");
+        result = strstr(command, "./shellexecvp exit");
         if (result == NULL) {
             system(command);
         } else {
